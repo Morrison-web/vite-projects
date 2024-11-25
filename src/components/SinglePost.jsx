7d85282fc49d8c0ef4c09a8f05cfda5784a0e203
Morrison-web <morrisonboateng566@gmail.com>
2024-11-25 12:20:19 -0300

    My first Project

--- src/components/SinglePost.jsx
+++ src/components/SinglePost.jsx
@@ -1,14 +1,16 @@
+import { Link } from "react-router-dom";
+
 export default function SinglePost(){
     return(
         <>
                      <div className="post-preview">
-                        <a href="post.html">
+                        <Link to="post.html">
                             <h2 className="post-title">Man must explore, and this is exploration at its greatest</h2>
                             <h3 className="post-subtitle">Problems look mighty small from 150 miles up</h3>
-                        </a>
+                        </Link>
                         <p className="post-meta">
                             Posted by
-                            <a href="#!">Start Bootstrap</a>
+                            <Link to="#!">Start Bootstrap</Link>
                             on September 24, 2023
                         </p>
                     </div>
